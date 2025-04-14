7418d9a619aee302e7cac6c2a6553b95904c7e35
Leo10401 <hunterleo626217@gmail.com>
2025-04-14 14:11:14 +0530

    commit

---
+++ frnt/src/app/page.jsx
@@ -0,0 +1,9 @@
+import EmailGenerator from "@/components/email-generator"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-slate-50 to-slate-100">
+      <EmailGenerator />
+    </main>
+  )
+}
